c9fb2317aec8408c03c3cc0144e789a6774e8d11
Josiah Ben <benjosiah90@gmail.com>
2022-12-09 12:58:20 +0100

    home page edit

--- public/build/assets/Dashboard.d7027ada.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./AppLayout.366f3093.js";import{o as t,c as e,w as o,a as s}from"./app.1f2cbd09.js";import"./_plugin-vue_export-helper.cdc0426e.js";const r=s("h2",{class:"font-semibold text-xl text-gray-800 leading-tight"}," Dashboard ",-1),m={__name:"Dashboard",setup(_){return(c,i)=>(t(),e(a,{title:"Dashboard"},{header:o(()=>[r]),_:1}))}};export{m as default};
